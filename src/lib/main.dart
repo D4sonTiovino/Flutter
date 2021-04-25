567035e30f947c1707fb978fac5d15bc771aac9f
Dason Tiovino <dason.6851@ski.sch.id>
2021-04-25 13:09:50 +0700

    Error fixed

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Text'),
+          title: Text('Welcome to Flutter')
         ),
         body: Center(
-          child: Text('Text') ,
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
